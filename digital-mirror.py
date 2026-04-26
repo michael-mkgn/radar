5e199fc734a7bdb24bb78c592d9e7956fc5e8075
fucks0ciety <zxc10007cursel@gmail.com>
2026-04-26 07:36:23 +0300

    Initial commit: Digital Mirror OSINT tool

---
+++ digital-mirror.py
@@ -0,0 +1,35 @@
+try:
+    import requests
+    
+    headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36" }
+
+
+    count = 0    
+    urls = ["https://vk.com/",
+            "https://vk.ru/",
+            "https://www.tiktok.com/@",
+            "https://www.facebook.com/",
+            "https://soundcloud.com/",
+            "https://x.com/",
+            "https://github.com/",
+            "https://www.instagram.com/",
+            "https://www.youtube.com/@",
+            ]
+    username = input("username for search\n>")
+    for url in urls:
+        full_url= (url+username)
+        response = requests.get(full_url, headers=headers, timeout=5)
+        if response.status_code == 200:
+            count += 1
+            print(f"found: {full_url}")
+
+    if count:
+        print(f"{count} links found")
+    else:
+        print("not links found ")
+
+
+
+except ModuleNotFoundError:
+    print("error: no module requests")
